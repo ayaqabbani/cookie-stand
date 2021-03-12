4a0d56b9dcb4d81e96d0abfaa8a316fd75ce11c8
ayaqabbani <ayaqabbani998@gmail.com>
2021-03-12 21:17:55 +0200

    added a form

--- js/yahya.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-let form=document.getElementById('myform');
-form.addEventListener('submit',myFunc);
-function myFunc(event){
-event.preventDefault();
-console.log(event.target.username.value);
-}
